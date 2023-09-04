7c0db6e777718c16c7edc3d9c6b3c86a3fd9f3e3
nick@nickcondon.com <124863043+nick-condon@users.noreply.github.com>
2023-09-05 08:15:48 +1000

    welcome section added

--- src/components/Main/Welcome.js
+++ src/components/Main/Welcome.js
@@ -1,12 +1,32 @@
 import React from "react";
+import './Welcome.css';
+import Overlay from "./Overlay";
+import WelcomeOverlay from "./WelcomeOverlay";
+import WelcomePhoto from '../../assets/images/welcome-tennis-court.jpg'
+import { useState } from "react";
 
 const Welcome = () => {
+
+    const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+
     return (
         <div className="Welcome">
-            <h1>Welcome</h1>
-            <h3>to Future Sports</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p>Mauris in aliquam sem fringilla ut morbi tincidunt id neque aliquam vestibulum.</p>
+            <div>
+                <img className="WelcomeImage" src={WelcomePhoto} alt="empty tennis court with net" />
+            </div>
+            <div className="WelcomeOverlayMessage">
+                <h1>Welcome</h1>
+                <h3 className="H3Welcome">to Future Sports</h3>
+                <p className="PWelcome">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p className="PWelcome">Mauris in aliquam sem fringilla ut morbi tincidunt id neque aliquam vestibulum.</p>
+                <button type="submit" class="welcome-btn" onClick={() => setIsOverlayOpen(!isOverlayOpen)}>Learn More &#8594;</button>
+            </div>
+            <Overlay
+            isOpen={isOverlayOpen}
+            onClose={() => setIsOverlayOpen(!isOverlayOpen)}
+            >
+                {WelcomeOverlay()}
+            </Overlay>
         </div>
     );
 };
