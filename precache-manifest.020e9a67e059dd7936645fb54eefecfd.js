9208830a421b1ee7360bbd9ed4fe7b68ce009b35
nick@nickcondon.com <124863043+nick-condon@users.noreply.github.com>
2023-09-13 15:03:58 +1000

    Updates

---
+++ precache-manifest.020e9a67e059dd7936645fb54eefecfd.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "6520f348224f3097bf5a",
+    "url": "/my-app/static/css/main.4a0e7da8.chunk.css"
+  },
+  {
+    "revision": "6520f348224f3097bf5a",
+    "url": "/my-app/static/js/main.6c5a3f93.chunk.js"
+  },
+  {
+    "revision": "3f092e5bf0ca544f064c",
+    "url": "/my-app/static/js/runtime~main.35145b17.js"
+  },
+  {
+    "revision": "c1f07e32d33e10f7d0d6",
+    "url": "/my-app/static/js/2.c5867bc2.chunk.js"
+  },
+  {
+    "revision": "c702756c9cb0ce7eb52f",
+    "url": "/my-app/static/js/3.d8ac33f5.chunk.js"
+  },
+  {
+    "revision": "2efa3c7101b4c6acccdbc1c5f14f7607",
+    "url": "/my-app/static/media/welcome-tennis-court.2efa3c71.jpg"
+  },
+  {
+    "revision": "121ebfff6a4edf7066637c2279317e30",
+    "url": "/my-app/static/media/services-1.121ebfff.jpg"
+  },
+  {
+    "revision": "a10488b30a36388456151c7aeedec5cd",
+    "url": "/my-app/static/media/services-2.a10488b3.jpg"
+  },
+  {
+    "revision": "994e43e2eed2b4fdf216cff9d684328a",
+    "url": "/my-app/static/media/services-3.994e43e2.jpg"
+  },
+  {
+    "revision": "418cb4df41f8f7beadb7dbebfb619fad",
+    "url": "/my-app/static/media/services-4.418cb4df.jpg"
+  },
+  {
+    "revision": "7da51ed821f17b2ec17af046282e6b18",
+    "url": "/my-app/static/media/slide-one-doubles.7da51ed8.jpg"
+  },
+  {
+    "revision": "5b6123e3a9c374597228410c5da77219",
+    "url": "/my-app/static/media/slide-2-balance-ball.5b6123e3.jpg"
+  },
+  {
+    "revision": "55712556620a2d98e6db4bf2e3eeec99",
+    "url": "/my-app/static/media/slide-3-tennis-rac.55712556.jpg"
+  },
+  {
+    "revision": "f5e282f425d01c46d455ddead8c1f3dd",
+    "url": "/my-app/static/media/gallery-image-1.f5e282f4.jpg"
+  },
+  {
+    "revision": "cb828a61ee5952c04a7cb909a2819edd",
+    "url": "/my-app/static/media/gallery-image-2.cb828a61.jpg"
+  },
+  {
+    "revision": "6f94a66c72065419ce09a6fddd4dd6d8",
+    "url": "/my-app/static/media/gallery-image-3.6f94a66c.jpg"
+  },
+  {
+    "revision": "d4f614382ac225510550f5e1e6119531",
+    "url": "/my-app/static/media/gallery-image-4.d4f61438.jpg"
+  },
+  {
+    "revision": "94a4b88044539a27d87efed297293281",
+    "url": "/my-app/static/media/gallery-image-5.94a4b880.jpg"
+  },
+  {
+    "revision": "d7d2f206c593133a4b065a217c2d4719",
+    "url": "/my-app/static/media/gallery-image-6.d7d2f206.jpg"
+  },
+  {
+    "revision": "c4dc987e613dfe13153589fc444cf548",
+    "url": "/my-app/static/media/gallery-image-7.c4dc987e.jpg"
+  },
+  {
+    "revision": "bc1a3466a782d4de78dbe40f1e3cfc24",
+    "url": "/my-app/static/media/gallery-image-8.bc1a3466.jpg"
+  },
+  {
+    "revision": "a913896675dbfdb8518dca0c4d5cf542",
+    "url": "/my-app/static/media/How-to-improve-your-return-of-serve.a9138966.mp4"
+  },
+  {
+    "revision": "9c250882c54a640b7030daba4cb0aab0",
+    "url": "/my-app/static/media/slide-shot.9c250882.jpg"
+  },
+  {
+    "revision": "6dc2e79b747f4a1b5d0a2bd52c539b0c",
+    "url": "/my-app/static/media/trick-shot.6dc2e79b.jpg"
+  },
+  {
+    "revision": "6228e90d2a1757f6330f6ac53c783c3d",
+    "url": "/my-app/static/media/underarm-serve.6228e90d.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b79331ff3d26df3044a1b8724e7ad7bc",
+    "url": "/my-app/static/media/contact-us.b79331ff.jpg"
+  },
+  {
+    "revision": "a1eb9e10b4979abf064cc72b4f655652",
+    "url": "/my-app/index.html"
+  }
+];
